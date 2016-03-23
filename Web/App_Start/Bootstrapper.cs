f081d07069fe17effcba158aa29e230b0001b88e
YeSv <yegor.sergeev@gmail.com>
2016-03-23 19:49:27 +0200

    Add simple version of Password and UserName revocery

--- Web/App_Start/Bootstrapper.cs
+++ Web/App_Start/Bootstrapper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Reflection;
+using System.Configuration; //needs for mailing service interface implementation
 using System.Web.Mvc;
 using Autofac;
+using Autofac.Core;
 using Autofac.Integration.Mvc;
 using BLL.Identity.Models;
 using BLL.Identity.Stores;
@@ -9,6 +11,8 @@ using DAL;
 using IDAL.Interfaces;
 using Microsoft.AspNet.Identity;
 
+using BLL.Services.MailingService;
+using BLL.Services.MailingService.Interfaces; 
 namespace Web
 {
     class Bootstrapper
@@ -25,6 +29,14 @@ namespace Web
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
             builder.RegisterType<UserStore>().As<IUserStore<IdentityUser, Guid>>().InstancePerRequest();
             builder.RegisterType<RoleStore>().As<IRoleStore<IdentityRole, Guid>>().InstancePerRequest();
+            builder.RegisterType<MailingService>().As<IMailingService>().WithParameters(
+                new Parameter[] {
+                    new NamedParameter("from", ConfigurationManager.AppSettings["mailFrom"]),
+                    new NamedParameter("password", ConfigurationManager.AppSettings["mailPass"]),
+                    new NamedParameter("host", ConfigurationManager.AppSettings["mailHost"])
+                })
+                .InstancePerRequest(); //configuration for mailing service
+
             IContainer container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -9,7 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 using Web.ViewModels;
 
-using BLL.Services.MailingService;
+using BLL.Services.MailingService.Interfaces;
+
 namespace Web.Controllers
 {
     public class HomeController : Controller
@@ -17,16 +18,16 @@ namespace Web.Controllers
         private readonly UserManager<IdentityUser, Guid> _userManager;
         private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
 
-        public HomeController(IUserStore<IdentityUser, Guid> userStore)
+        public HomeController(IUserStore<IdentityUser, Guid> userStore, IMailingService emailService)
         {
             _userManager = new UserManager<IdentityUser, Guid>(userStore);
-            _userManager.EmailService = new MailingService();
+            _userManager.EmailService = emailService;
             
         }
         // GET: Home
         public ActionResult Index()
         {
-            MailingService srv = new MailingService();
+            //MailingService srv = new MailingService();
             return View();
         }
 
@@ -58,6 +59,144 @@ namespace Web.Controllers
             
         }
 
+
+        //password remindering methods
+        [HttpGet]
+        [AllowAnonymous]
+        public ViewResult RemindUserPassword()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [HandleError(ExceptionType = typeof(HttpAntiForgeryException), View = "AntiForgeryError")]
+        public async Task<ActionResult> RemindUserPassword(String userName)
+        {
+            String errorSummary = String.Empty; //summary error message
+
+            if (!ModelState.IsValid)
+                errorSummary = "Some message on dutch about error with user's name!";
+            else
+            {
+                IdentityUser user = await _userManager.FindByNameAsync(userName); //searching user by name
+
+                if (user == null) //user not found
+                    errorSummary = "Can't find user. Try later or register.";
+                else
+                {
+
+                    String passResetToken = await _userManager.GeneratePasswordResetTokenAsync(user.Id); //generating password reset token
+
+                    if (!String.IsNullOrEmpty(passResetToken)) //generation of token failed
+                        errorSummary = "Server Error. Try later";
+                    else
+                    {
+                        String callbackUrl = Url.Action("PasswordRecovery", "Home", new { userId = user.Id, token = passResetToken }, Request.Url.Scheme); //sets recovery url
+                        await _userManager.SendEmailAsync(user.Id, "Password recovery", $"To recover your password <a href=\"{callbackUrl}\" target=\"_blank\">click here</a>");
+                        return RedirectToAction("RemindUserPasswordSuccess"); //succeeded
+                    }
+                }
+            }
+
+            //error occured
+            ModelState.AddModelError("", errorSummary);
+            return View();            
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ViewResult RemindUserPasswordSuccess()
+        {
+            return View();
+        }
+
+
+        //username changing methods
+        [HttpGet]
+        [AllowAnonymous]
+        public ViewResult RemindUserName()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [HandleError(ExceptionType = typeof(HttpAntiForgeryException), View = "AntiForgeryError")]
+        public async Task<ActionResult> RemindUserName(String email)
+        {
+            String errorSummary = String.Empty; //summary error message
+
+            if (!ModelState.IsValid)
+                errorSummary = "Some message on dutch about error with user's email!";
+            else
+            {
+                IdentityUser user = await _userManager.FindByEmailAsync(email); //searching user by name
+
+                if (user == null) //user not found
+                    errorSummary = "Can't find user. Try later or register.";
+                else
+                {
+                    await _userManager.SendEmailAsync(user.Id, "User name remindering", $"Hello, dear user. Your username is {user.UserName}");
+                    return RedirectToAction("RemindUserNameSuccess"); //succeeded
+                }
+            }
+
+            ModelState.AddModelError("", errorSummary);
+            return View();
+        }
+ 
+        [HttpGet]
+        [AllowAnonymous]
+        public ViewResult RemindUserNameSuccess()
+        {
+            return View();
+        }
+
+        //password recovery (after remindering and revieving of email with token)
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult PasswordRecovery(Guid id, String token)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "URL for reset is invalid.");
+                return RedirectToAction("RemindUserPassword");
+            }
+            return View(new { Id = id, Token = token });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [HandleError(ExceptionType = typeof(HttpAntiForgeryException), View = "AntiForgeryError")]
+        public async Task<ActionResult> PasswordRecovery(String pass, String confPass, Guid id, String token)
+        {
+            String errorMessage = String.Empty; //error message
+
+            if (String.CompareOrdinal(pass, confPass) != 0)  //comapres passwords if not match
+                errorMessage = "Password and confirmation password are not match";
+            else //if match
+            {
+                if (!ModelState.IsValid && !String.IsNullOrWhiteSpace(pass) && !String.IsNullOrWhiteSpace(confPass)) //if not valid data and passwords not null or white space, etc..
+                    errorMessage = "Error with reseting url. Try to generate new token for confirmation.";
+                else //if data valid
+                {
+                    var result = await _userManager.ResetPasswordAsync(id, token, pass); //reseting password
+
+                    if (result.Succeeded) 
+                        return RedirectToAction("Index"); //redirecting to index if succeded
+                    else
+                        errorMessage = "Error with reseting password, please, try later. Or generate new token for confirmation."; //password reset 
+                }
+            }
+            //error occured
+            ModelState.AddModelError("", errorMessage);
+            return View();
+        }
 
         private async Task SignInAsync(IdentityUser user, bool isPersistent)
         {
