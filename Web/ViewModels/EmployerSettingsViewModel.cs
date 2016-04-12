432aa01812d8bf81c470ab91f34fa91b5009078f
SergYevtukhoff <serg.yevtukhoff@yandex.ru>
2016-04-12 23:55:12 +0300

    Include my local untracked file for pulling with Egor

---
+++ Web/ViewModels/EmployerSettingsViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels
+{
+    public class EmployerSettingsViewModel
+    {
+        [Required]
+        [Display(Name = "Bedrijfsnaam")]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [Display(Name = "Voornaam")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Tussenvoegsel")]
+        public string Prefix { get; set; }
+
+        [Required]
+        [Display(Name = "Achternaam")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Telefoon")]
+        public string TelephoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email adres")]
+        [EmailAddress]
+        public string EmailAdress { get; set; }
+
+        [Required]
+        [Display(Name = "Postcode")]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [Display(Name = "Adres")]
+        public string Adress { get; set; }
+
+        [Required]
+        [Display(Name = "Plaats")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Gebruikersnaam")]
+        public string UserName { get; set; }
+    }
+}
