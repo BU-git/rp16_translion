0b40c9354afe5b720c2c076cdbe5c8687e083344
YeSv <yegor.sergeev@gmail.com>
2016-04-13 12:50:21 +0300

    Add untracked files

---
+++ BLL/Services/MailingService/MailMessageBuilders/EmployerDelEmployeeMessageBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.MailingService.MailMessageBuilders
+{
+    public class EmployerDelEmployeeMessageBuilder : MailMessageBuilder
+    {
+        private readonly string _body = "Hello. Employer {0} deleted employee {1}.";
+        private readonly string _subject = "Employee deletion";
+
+        public EmployerDelEmployeeMessageBuilder(string employerName, string employeeName)
+        {
+            Body = String.Format(_body, employerName, employeeName);
+            Subject = _subject;
+        }
+    }
+}
