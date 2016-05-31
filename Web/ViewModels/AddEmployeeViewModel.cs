24b634151eaf4a048e7cb37f3e5537cbd941abc1
Myskiv Viktor <myskiv.viktor@gmail.com>
2016-05-31 17:45:03 +0300

    Fix TRAN-84

--- Web/ViewModels/AddEmployeeViewModel.cs
+++ Web/ViewModels/AddEmployeeViewModel.cs
@@ -8,20 +8,20 @@ namespace Web.ViewModels
         public Guid EmployerID { get; set; }
         [Required(ErrorMessage = "Vul voornaam in")]
         [Display(Name = "Voornaam")]
-        [StringLength(15, ErrorMessage = "Too long username")]
+        [StringLength(15, ErrorMessage = "Gebruikersnaam te lang")]
         [RegularExpression("[a-zA-Z]+",
             ErrorMessage = "Veld klopt niet, controleer dit aub. Nummers, symbolen en spaties zijn niet toegestaan")]
         public string FirstName { get; set; }
 
         [Display(Name = "Tussenvoegsel")]
-        [StringLength(5, ErrorMessage = "Too long prefix")]
-        [RegularExpression("[a-zA-Z]+",
+        [StringLength(20, ErrorMessage = "Tussenvoegsel te lang")]
+        [RegularExpression("[a-zA-Z\\s]+",
             ErrorMessage = "Veld klopt niet, controleer dit aub. Nummers, symbolen en spaties zijn niet toegestaan")]
         public string Prefix { get; set; }
 
         [Required(ErrorMessage = "Vul achternaam in")]
         [Display(Name = "Achternaam")]
-        [StringLength(15, ErrorMessage = "Too long lastname")]
+        [StringLength(15, ErrorMessage = "Achternaam te lang")]
         [RegularExpression("[a-zA-Z]+",
            ErrorMessage = "Veld klopt niet, controleer dit aub. Nummers, symbolen en spaties zijn niet toegestaan")]
         public string LastName { get; set; }
