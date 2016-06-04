26cc6c375f11dfc5bac1c61757e6e61596e7289f
YeSv <yegor.sergeev@gmail.com>
2016-06-04 15:51:58 +0300

    Add admin functionality for employer password recovery

---
+++ Web/ViewModels/AdminResetEmployerPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels
+{
+    public sealed class AdminResetEmployerPasswordViewModel
+    {
+        [Required]
+        public string ResultMessage { get; set; }
+    }
+}
